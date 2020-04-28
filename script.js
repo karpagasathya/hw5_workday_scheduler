dceaeafa0358011ee4beca3283c796658b63dec8
sathya <karpagasathya@gmail.com>
2020-04-28 18:29:33 -0500

    started html and js file

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+    $(".saveBtn").on("click", function () {
+        console.log(this);
+        var text = (".description").val();
+        var time=
+    });   
+})
+
