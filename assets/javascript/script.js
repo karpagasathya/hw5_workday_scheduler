77b6ee1b827f664b9d7546519e792d64293bb524
sathya <karpagasathya@gmail.com>
2020-05-01 00:56:12 -0500

    for loop in js file

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,50 +1,41 @@
 $(document).ready(function () {
-    $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
-    $(".saveBtn").on("click", function () {
-        // referred this link for siblings() https://api.jquery.com/siblings/
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
+  $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+  $(".saveBtn").on("click", function () {
+    // referred this link for siblings() https://api.jquery.com/siblings/
+    var text = $(this).siblings(".description").val().trim();
+    var time = $(this).parent().attr("id");
 
-        localStorage.setItem(time, text);
+    // skip save to local storage when textarea value is null
+    if (text !== "") {
+      localStorage.setItem(time, JSON.stringify(text));
+    }
+  });
+
+  for (let i = 0; i < localStorage.length; i++) {
+    let timeblocksIdDescription = "#" + localStorage.key(i) + " .description";
+    $(timeblocksIdDescription).val(JSON.parse(localStorage.getItem(localStorage.key(i))));
+  }
+
+  function hourTracker() {
+    var currentHour = moment().hour();
+    $(".time-block").each(function () {
+      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
+      var scheduleHour = parseInt($(this).attr("id").split("hour")[1]);
+
+      if (scheduleHour < currentHour) {
+        $(this).addClass("past");
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+      } else if (scheduleHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+        $(this).removeClass("future");
+      } else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+      }
     });
-    
-    let timeBlock = ["#hour9", "#hour10", "#hour11", "#hour12", "#hour13", "#hour14", "#hour15", "#hour16", "#hour17"];
-    for (let i = 0; i < timeBlock.length; i++) { 
-        $(timeBlock[i]).val(localStorage.getItem(timeBlock[i]));
-    }
-    
-//    $("#hour9 .description").val(localStorage.getItem("hour9"));
-//    $("#hour10 .description").val(localStorage.getItem("hour10"));
-//    $("#hour11 .description").val(localStorage.getItem("hour11"));
-//    $("#hour12 .description").val(localStorage.getItem("hour12"));
-//    $("#hour13 .description").val(localStorage.getItem("hour13"));
-//    $("#hour14 .description").val(localStorage.getItem("hour14"));
-//    $("#hour15 .description").val(localStorage.getItem("hour15"));
-//    $("#hour16 .description").val(localStorage.getItem("hour16"));
-//    $("#hour17 .description").val(localStorage.getItem("hour17"));
-    
-    function hourTracker() {
-        var currentHour = moment().hour();
-        $(".time-block").each(function () {
-            // gf https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
-            var scheduleHour = parseInt($(this).attr("id").split("hour")[1]);
-            console.log(scheduleHour, currentHour);
-            if (scheduleHour < currentHour) {
-              $(this).addClass("past");
-              $(this).removeClass("future");
-              $(this).removeClass("present");
-            } else if (scheduleHour === currentHour) {
-              $(this).removeClass("past");
-              $(this).addClass("present");
-              $(this).removeClass("future");
-            } else {
-              $(this).removeClass("present");
-              $(this).removeClass("past");
-              $(this).addClass("future");
-            }
-        })
-
-    }
-hourTracker()
-})
-
+  }
+  hourTracker();
+});
