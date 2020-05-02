3bc0aceeff40cda843b5fcb3bb47a01cb0caf600
sathya <karpagasathya@gmail.com>
2020-05-02 10:05:07 -0500

    worked on js code and html

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# hw5_workday_scheduler
+# Work Day Scheduler
+
+## Overview
+A simple day planner application that allows the user to save events for each hour of their standard work day. In addition the app display's standard business hours (9 a.m. to 5 p.m.) along the left hand side of each input row. Depending on the time of day, the schedule input fields update their color indicating to the user whether items are in the past, present or future. 
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,19 +1,53 @@
 $(document).ready(function () {
   $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+
+  var date = moment().format("LL");
+  var scheduleArr = [];
+  var schedule = {
+    hourId: "",
+    hourText: "",
+  };
+
+  displaySchedule();
+
   $(".saveBtn").on("click", function () {
-    // referred this link for siblings() https://api.jquery.com/siblings/
     var text = $(this).siblings(".description").val().trim();
     var time = $(this).parent().attr("id");
+    var storedSchedules = JSON.parse(localStorage.getItem(date));
 
-    // skip save to local storage when textarea value is null
+    // checking if the plan text is not null
     if (text !== "") {
-      localStorage.setItem(time, JSON.stringify(text));
+      // checking if this is first time entry for the day and add it to local storage
+      if (storedSchedules === null) {
+        schedule.hourId = time;
+        schedule.hourText = text;
+        scheduleArr.push(schedule);
+
+        localStorage.setItem(date, JSON.stringify(scheduleArr));
+      } else {
+        let scheduleIndex = storedSchedules.findIndex((storedSchedule) => storedSchedule.hourId === time);
+        // checking if hour plan already exists and update the text else add it to the storage
+        if (scheduleIndex !== -1) {
+          storedSchedules[scheduleIndex].hourText = text;
+        } else {
+          schedule.hourId = time;
+          schedule.hourText = text;
+          storedSchedules.push(schedule);
+        }
+
+        localStorage.setItem(date, JSON.stringify(storedSchedules));
+      }
     }
   });
 
-  for (let i = 0; i < localStorage.length; i++) {
-    let timeblocksIdDescription = "#" + localStorage.key(i) + " .description";
-    $(timeblocksIdDescription).val(JSON.parse(localStorage.getItem(localStorage.key(i))));
+  function displaySchedule() {
+    savedSchedule = JSON.parse(localStorage.getItem(date));
+    if (savedSchedule !== null) {
+      for (var i = 0; i < savedSchedule.length; i++) {
+        let timeblocksIdDescription = "#" + savedSchedule[i].hourId + " .description";
+        $(timeblocksIdDescription).val(savedSchedule[i].hourText);
+      }
+    }
   }
 
   function hourTracker() {
